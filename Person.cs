dcdccd7f43cd1ca33d843a60462e1f9e57e97d5e
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-13 15:46:05 +0530

    Added Create Contact Method

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public static class Person
+    {
+        public static void CreatePerson()
+        {
+            Console.Write("Enter First Name: ");
+            string Firstname = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string Lastname = Console.ReadLine();
+
+            Console.Write("enter the Phone number: ");
+            long Phonenumber = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the zipcode: ");
+            int Zipcode = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the city: ");
+            string City = Console.ReadLine();
+        }
+    }
+}
