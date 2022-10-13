d5d8b7290d56efa148779d441e2fcbe9a80cfa4f
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-13 15:55:24 +0530

    Added Adding Contact Method

---
+++ Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Contact
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public long Phonenumber { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+    }
+}
