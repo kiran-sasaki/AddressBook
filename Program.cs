432a5cf20debbcbe82d0a350ae00744321f33a72
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-03 16:23:25 +0530

    added Search Person Method

--- Program.cs
+++ Program.cs
@@ -8,37 +8,31 @@
             Person person = new Person();
             while (true)
             {
-                Console.WriteLine("Please enter a command: ");
-                string command = Console.ReadLine();
-                Console.WriteLine("Entered Command is "+command);
-
-                if (command == "add")
+                Console.WriteLine("Enter number: \n 1. create new person \n 2. Display Data \n 3. Edit person Data \n 4. Delete Person Data \n 5. Add Multiple Contacts \n 6.Add With UniqueBookName \n 7.Search For Duplicate \n 8.Sreach By CityName");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
                 {
-                    Person.AddPerson();
-                }
-                if(command == "view")
-                {
-                    Person.listPerson();
-                }
-                if (command == "edit")
-                {
-                    Person.EditPerson();
-                }
-                if(command == "delete")
-                {
-                    Person.DeleteContact();                
-                }
-                if(command == "AddMultiplePerson")
-                {
-                    Person.MultipleContacts();
-                }
-                if(command == "UniqueBookName")
-                {
-                    person.NewUser();
-                }
-                if(command == "ViewDictionary")
-                {
-                    person.Display();
+                    case 1:
+                        Person.AddPerson();
+                        break;
+                    case 2:
+                        Person.listPerson();
+                        break;
+                    case 3:
+                        Person.EditPerson();
+                        break;
+                    case 4:
+                        Person.DeleteContact();
+                        break;
+                    case 5:
+                        Person.MultipleContacts();
+                        break;
+                    case 6:
+                        person.NewUser();
+                        break;
+                    case 7:
+                        person.DuplicateContact();
+                        break;
                 }
             }
         }
